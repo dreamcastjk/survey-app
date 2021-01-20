6515c560335472345b0ebe869d2a07ac29c05caa
Zhiharev <slipdoll@gmail.com>
2021-01-20 17:05:03 +0300

    home view for list of questionnaires

--- app/Models/Questionnaire.php
+++ app/Models/Questionnaire.php
@@ -4,12 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Questionnaire extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function getPublicPathAttribute()
+    {
+        return route('survey.show', [$this, Str::slug($this->title)]);
+    }
+
+    public function getPathAttribute()
+    {
+        return route('questionnaire.show', $this);
+    }
 
     public function user()
     {
